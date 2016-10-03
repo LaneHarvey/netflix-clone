96cfb694e1c2e8f9ed25553a762c78337b7b5486
Lane Harvey <mrlaneharvey@gmail.com>
2016-10-03 13:49:28 -0600

    created necessary files and directories

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+angular.module('app', ['ai-router'])
+.config(function($stateProvider, $urlRouterProvider) {
+$urlRouterProvider.otherwise('/');
+$stateProvider
+.state('', {
+})
+})
---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var massive = require('massive');
+var connectionString = "postgres://postgres:@localhost/netflix-clone";
+
+var app = express();
+module.exports = app;
+
+var massiveInstance = massive.connectSync({connectionString : connectionString});
+
+app.set('db', massiveInstance);
+
+// var db = app.get('db');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('../public'));
+
+var mainCtrl = require('./controllers/mainCtrl');
+
+
+
+var port = 9001;
+app.listen(port, function(){
+  console.log("It's working on ", port);
+});
